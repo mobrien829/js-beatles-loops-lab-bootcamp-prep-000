8015e41bf9efaa7566b4b10a91fc6cf418fb47bb
Michael O%27Brien <mobrien@colgate.edu>
2018-05-03 02:16:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay (musicians, instruments) {
+  var roles = []
+  for (let i = 0; i < 4; i++) {
+  roles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return roles
+}
+
+
+function johnLennonFacts (facts) {
+  let n = 3
+  while (n > 0) {
+  return console.log([`${facts[n--]}` + "!!!"])
+  }
+}
+
+function iLoveTheBeatles (n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+  } while (n < 15)
+}
+
+
+//  for theBeatlesPlay() needs to be i is 0-3
